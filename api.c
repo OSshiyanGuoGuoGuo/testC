7636763b2a32bd8e952dd934ce91315a79d79a81
WZJun <128673164+OSshiyanGuoGuoGuo@users.noreply.github.com>
2024-08-15 16:03:47 +0800

    Add files via upload

--- api.c
+++ api.c
@@ -1,28 +1,14 @@
 #include <stdio.h>
 
-// rename
-void displayMessage(const char *message) {
+void printMessage(const char *message) {
     printf("%s\n", message);
 }
 
-// delete
-// void addNumbers(int a, int b) {
-//     printf("Sum: %d\n", a + b);
-// }
-
-// change signature
-void sumAndDisplay(int number1, int number2) {
-    printf("Total: %d\n", number1 + number2);
-}
-
-// add method
-void addPrint() {
-    printf("This a new method\n");
+void addNumbers(int a, int b) {
+    printf("Sum: %d\n", a + b);
 }
 
 int main() {
-
-    sumAndDisplay(5, 10);
-    addPrint();
+    addNumbers(5, 10);
     return 0;
 }
